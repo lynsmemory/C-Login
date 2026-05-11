80bd7e50f7e364a2d60f970c17ba24c33ebabb6c
lynsmemory <thing0831@naver.com>
2026-05-11 18:10:18 +0900

    Login Project

---
+++ 20260511/scrCreateAccount.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scrCreateAccount : MonoBehaviour
+{
+    //신규 계정 만들기
+    //아이디, 패스워드를 리스트로 저장하기
+    //인풋필드에 입력된 값을 가져와야한다.
+    //로그인 버튼을 누르면 값을 전송한다.
+
+    public InputField id = null;
+    public InputField password = null;
+    public InputField recheck = null;
+
+    public void CreateAccount()
+    {
+            string inputID = id.text;
+            string inputPassword = password.text;
+            string inputRecheck = recheck.text;
+
+        if (inputID == "")
+        {
+            Debug.Log("아이디를 입력하십시오.");
+            return;
+        }
+
+        else if (inputPassword == "")
+        {
+            Debug.Log("패스워드를 입력하십시오.");
+            return;
+        }
+
+        else if (inputPassword != inputRecheck)
+        {
+            Debug.Log("입력하신 비밀번호가 다릅니다.");
+            return;
+        }
+        else
+        {
+            Debug.Log("알 수 없는 오류입니다.");
+        }
+
+        UserDatabase.ids.Add(inputID);
+        UserDatabase.passwords.Add(inputPassword);
+
+        Debug.Log("회원가입 완료");
+
+        Debug.Log("저장된 아이디 : " + UserDatabase.ids[0]);
+        Debug.Log("저장된 비밀번호 : " + UserDatabase.passwords[0]);
+
+        SceneManager.LoadScene("Login");
+    }
+
+
+    
+    
+}
---
+++ 20260511/scrLogin.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scrLogin : MonoBehaviour
+{
+    public InputField loginID;
+    public InputField loginPassword;
+
+    // 로그인 버튼
+    public void Login()
+    {
+        string inputID = loginID.text;
+        string inputPassword = loginPassword.text;
+
+        bool success = false;
+
+        // 저장된 계정 검사
+        for (int i = 0; i < UserDatabase.ids.Count; i++)
+        {
+            if (inputID == UserDatabase.ids[i] &&
+               inputPassword == UserDatabase.passwords[i])
+            {
+                success = true;
+                break;
+            }
+        }
+
+        // 결과
+        if (success == true)
+        {
+            Debug.Log("로그인 성공");
+
+            // 다음 씬 이동
+            SceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            Debug.Log("아이디 또는 비밀번호가 틀렸습니다.");
+        }
+    }
+
+    // 회원가입 씬 이동 버튼
+    public void GoToRegister()
+    {
+        SceneManager.LoadScene("CreateAccount");
+    }
+}
